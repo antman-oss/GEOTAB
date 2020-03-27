bf58724e818451a2077c8d7b3fb6ceaf9401d1f7
antman-oss <antman1976@gmail.com>
2020-03-28 09:06:19 +1100

    Update UI Format
    Change layout of Controls. Changed behvavior of Filter Search (now requires * wildcards).

--- index.js
+++ index.js
@@ -70,35 +70,39 @@ app.post('/v1/file_upload/:sample', function(req, res, next){
         }
         
         for (const file of Object.entries(files)) {
-            uploadedFile = file[1].name
-            var contents = JSON.parse(fs.readFileSync(file[1].path));
-            var json_template = {"type":"FeatureCollection", "features": []} //GeoJSON Standard 2016
-            sampleArray = []
-            if (contents['type'] == "FeatureCollection" && contents['features'].length > 0){
-                              
-                var sampleCount = 0;
-                contents.features.forEach(element => {
-                    //Scan 
-                    const ele_obj = {}
-                    for (const kv_pair of Object.entries(element.properties)) {
-                        ele_obj[kv_pair[0]] = kv_pair[1];
-                    }
-                    for (const kv_pair of Object.entries(element.geometry)) {
-                        ele_obj[kv_pair[0]] = kv_pair[1];
-                    }
-                    ++sampleCount
-                    sampleArray.push(ele_obj)
+            try{
+                uploadedFile = file[1].name
+                var contents = JSON.parse(fs.readFileSync(file[1].path));
+                var json_template = {"type":"FeatureCollection", "features": []} //GeoJSON Standard 2016
+                sampleArray = []
+                if (contents['type'] == "FeatureCollection" && contents['features'].length > 0){
+                                
+                    var sampleCount = 0;
+                    contents.features.forEach(element => {
+                        //Scan 
+                        const ele_obj = {}
+                        for (const kv_pair of Object.entries(element.properties)) {
+                            ele_obj[kv_pair[0]] = kv_pair[1];
+                        }
+                        for (const kv_pair of Object.entries(element.geometry)) {
+                            ele_obj[kv_pair[0]] = kv_pair[1];
+                        }
+                        ++sampleCount
+                        sampleArray.push(ele_obj)
 
-                })
-                sourceFields(sampleArray[0])
-                response = {"msg": `File upload Successful. [${sampleCount} records]` ,
-                        "rows": sampleArray.slice(0,sampleSize)
+                    })
+                    sourceFields(sampleArray[0])
+                    response = {"msg": `File upload Successful. [${sampleCount} records]`,
+                            "update": true,
+                            "rows": sampleArray.slice(0,sampleSize)
+                    }
+                    res.send(response)
+                }else{
+                    res.sendStatus(407)
                 }
-                res.send(response)
-            }else{
+            }catch(e){
                 res.sendStatus(407)
             }
-
         }
       })
       
@@ -110,15 +114,48 @@ app.post("/v1/filter/:sample", function(req, res, next){
     const keyname = req.body.keyname
     var keyvalue = req.body.keyvalue; keyvalue = keyvalue ? keyvalue.split(',') : null;
     const limit = req.body.limit
-    const filterExport = req.body.export
+    const filter_export = req.body.filter_export
+    const exportType = req.body.export
     const exportTab = req.body.tab
     
     function filterCheck(fieldvalue,fieldfilter){
         var searchcount = 0;
         for(var i=0;i<fieldfilter.length;i++){
-            var reg = new RegExp(fieldfilter[i],'i')
-            if(fieldvalue.search(reg) > -1){
-                searchcount++
+            var searchType;
+            if(fieldfilter[i].slice(0,1) == '*' && fieldfilter[i].slice(-1) == '*'){
+                searchType = 1
+            }else if(fieldfilter[i].slice(-1) == '*'){
+                searchType = 2
+            }else if(fieldfilter[i].slice(0,1) == '*'){
+                searchType = 3
+            }else{
+                searchType =4
+            }
+            var term = fieldfilter[i].replace(/\*/g,'')
+            var strLen = fieldvalue.length
+            var termLen = term.length
+            var search = fieldvalue.search(term)
+            switch(searchType){
+                case 1:
+                    if(search > 0){
+                        searchcount++
+                    }
+                break;
+                case 2:
+                    if(search == 0){
+                        searchcount++
+                    }
+                break;
+                case 3:
+                    if(search >= 0 && search == (strLen - termLen)) {
+                        searchcount++
+                    }
+                break;
+                case 4:
+                    if(search == 0 && strLen == termLen){
+                        searchcount++
+                    }
+                break;
             }
         }
         if (searchcount > 0){ 
@@ -134,6 +171,7 @@ app.post("/v1/filter/:sample", function(req, res, next){
         
         if (exportNo == 1){ // Ensure we only response once
             response = {"msg": `Export Successful ${exportName}` ,
+                        "update": true,
                         "rows": filterArray.slice(0,sampleSize)
                 }
             res.send(response)
@@ -156,8 +194,9 @@ app.post("/v1/filter/:sample", function(req, res, next){
         console.log('Creating ' + exportName+'.txt')
 
         if (exportNo == 1){ // Ensure we only response once
-            response = {"msg": `Export Successful. First Export is ${exportName}.txt [${exportNo} files]` ,
-                        "rows": filterArray.slice(0,sampleSize)
+            response = {"msg": `Export Successful. First Export is ${exportName}.txt [${exportNo} files]`,
+                        "update": false,
+                        "rows": ''
                 }
             res.send(response)
         }
@@ -165,85 +204,79 @@ app.post("/v1/filter/:sample", function(req, res, next){
 
     var filterArray = []
     const geojson_template = {"type":"FeatureCollection", "features": []} 
-
-    //Filter
-    var filterCount=0
-    sampleArray.forEach(item =>{
-        
-        if (keyname && keyvalue){ //Filter
-            try{
-                var fieldvalue = eval('item.'+keyname);
-            }catch(e){
-                console.log('JSON Key does not exist ' + field)
-                res.sendStatus(405)
-            } 
-            if (filterCheck(fieldvalue,keyvalue)){
-                filterArray.push(item)
-                ++filterCount
-            }
-        }else{
-            filterArray = sampleArray
-        }
-    })
     
-    if(filterExport == 1){
+    if(filter_export == 1){
+        //Filter
+        var filterCount=0
+        sampleArray.forEach(item =>{
+            
+            if (keyname && keyvalue){ //Filter
+                try{
+                    var fieldvalue = eval('item.'+keyname);
+                }catch(e){
+                    console.log('JSON Key does not exist ' + field)
+                    res.sendStatus(405)
+                } 
+                if (filterCheck(fieldvalue,keyvalue)){
+                    filterArray.push(item)
+                    ++filterCount
+                }
+            }else{
+                filterArray = sampleArray
+            }
+        })
+    
         response = {"msg": `Filter Successful. [${filterCount} Records]`,
+                    "update": true,
                     "rows": filterArray.slice(0,sampleSize)
                 }
         res.send(response) // Return only sample size
-    }
-    
-    
-    //Export
-    var limitCount = 0;
-    separateFileCount = 0
-    filterArray.forEach(item =>{
-        
-        if (++limitCount <= limit || limit == 0){
-            //Append to buffer
-            var obj = {
-                "type": "Feature",
-                "geometry": {},
-                "properties":{}
-            }
-            const tmp_item = Object.assign({},item)
-            Object.assign(obj.geometry, {'type': tmp_item.type})
-            Object.assign(obj.geometry, {'coordinates': tmp_item.coordinates})
-            delete tmp_item.type
-            delete tmp_item.coordinates
+    }else{
+        //Export
+        var limitCount = 0;
+        separateFileCount = 0
+        filterArray.forEach(item =>{
             
-            Object.assign(obj.properties, tmp_item)
-            //combined files + limit
-            if (filterExport > 1){
-                // Append record to master
-                geojson_template.features.push(obj)
-            }
-
-            // Separated Export - run after every object
-            if (filterExport == 3){
-                if (exportTab){
-                    tabArray = [item]
-                    exportTabFile(tabArray,`export/limited_${limitCount}_${uploadedFile}`,++separateFileCount)
-                }else{
-                    exportFile(geojson_template,`export/limited_${limitCount}_${uploadedFile}`,++separateFileCount)
+            if (++limitCount <= limit || limit == 0){
+                //Append to buffer
+                var obj = {
+                    "type": "Feature",
+                    "geometry": {},
+                    "properties":{}
                 }
-                    // reset
-                geojson_template.features=[]
-                tabArray = []
+                const tmp_item = Object.assign({},item)
+                Object.assign(obj.geometry, {'type': tmp_item.type})
+                Object.assign(obj.geometry, {'coordinates': tmp_item.coordinates})
+                delete tmp_item.type
+                delete tmp_item.coordinates
+                
+                Object.assign(obj.properties, tmp_item)
+                geojson_template.features.push(obj)
+                
+                // Separated Export - run after every object
+                if (exportType){
+                    if (exportTab){
+                        tabArray = [item]
+                        exportTabFile(tabArray,`export/limited_${limitCount}_${uploadedFile}`,++separateFileCount)
+                    }else{
+                        exportFile(geojson_template,`export/limited_${limitCount}_${uploadedFile}`,++separateFileCount)
+                    }
+                        // reset
+                    geojson_template.features=[]
+                    tabArray = []
+                }
+            }
+        })
+
+        // Combined Export - run after all object appended
+        if (!exportType){
+            if (exportTab){
+                exportTabFile(filterArray,`export/combined_${limit}_${uploadedFile}`,1)
+            }else{
+                exportFile(geojson_template,`export/combined_${limit}_${uploadedFile}`,1)
             }
         }
-
-    })
-
-    // Combined Export - run after all object appended
-    if (filterExport == 2){
-        if (exportTab){
-            exportTabFile(filterArray,`export/combined_${limit}_${uploadedFile}`,1)
-        }else{
-            exportFile(geojson_template,`export/combined_${limit}_${uploadedFile}`,1)
-        }
-    }
-
+    }       
 });
 
 //Easier Exit for non-node users. Press q to exit
